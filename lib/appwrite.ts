1d8e967af4b9ffe29390098897fc0e60ab1509db
aessary <raressilviulazar@gmail.com>
2025-07-08 21:24:54 +0100

    Search Screen

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -99,3 +99,33 @@ export const logout = async () => {
         throw new Error(e as string);
     }
 }
+export const getMenu = async ({ category, query }: GetMenuParams) => {
+    try {
+        const queries: string[] = [];
+
+        if(category) queries.push(Query.equal('categories', category));
+        if(query) queries.push(Query.search('name', query));
+
+        const menus = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.menuCollectionId,
+            queries,
+        )
+
+        return menus.documents;
+    } catch (e) {
+        throw new Error(e as string);
+    }
+}
+export const getCategories = async () => {
+    try {
+        const categories = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.categoriesCollectionId,
+        )
+
+        return categories.documents;
+    } catch (e) {
+        throw new Error(e as string);
+    }
+}
