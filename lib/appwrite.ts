af70b3a3b9ed711f47735c3713073d219da3a0ef
aessary <raressilviulazar@gmail.com>
2025-07-08 14:49:05 +0100

    Appwrite Setup

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+
+
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
+    platform: "com.aessar.FoodOrderRN25July",
+}
