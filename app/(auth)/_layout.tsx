94075356246a8b45cf160640864130914ba5443b
aessary <raressilviulazar@gmail.com>
2025-07-08 15:44:13 +0100

    Auth Screens

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,32 @@
-import {View, Text} from 'react-native'
-import React from 'react'
-import {Slot} from "expo-router";
-import {SafeAreaView} from "react-native-safe-area-context";
+import {View, KeyboardAvoidingView, Platform, ScrollView, Dimensions, ImageBackground, Image} from 'react-native'
+import { Slot} from "expo-router";
+import {images} from "@/constants";
+import CustomInput from "@/components/CustomInput";
+import CustomButton from "@/components/CustomButton";
 
-export default function _Layout() {
+
+export default function AuthLayout() {
     return (
-        <SafeAreaView>
-            <Text>Auth Layout</Text>
-            <Slot />
-        </SafeAreaView>
+        // This component adjusts the view when the keyboard is visible.
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            {/* ScrollView enables content to be scrollable if it exceeds the screen height. */}
+            <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+                {/* A container for the top graphic, with its height dynamically calculated. */}
+                <View className="w-full relative" style={{ height: Dimensions.get('screen').height / 2.25}}>
+                    {/* The background image for the authentication screens. */}
+                    <ImageBackground
+                        source={images.loginGraphic}
+                        className="size-full rounded-b-lg"
+                        resizeMode="stretch"
+                    />
+                    {/* The application logo, positioned absolutely over the background. */}
+                    <Image source={images.logo} className="self-center size-48 absolute -bottom-16 z-10" />
+                </View>
+
+
+                {/* The Slot component renders the content of the child route (sign-in or sign-up). */}
+                <Slot />
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -4,4 +4,7 @@ export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
     platform: "com.aessar.FoodOrderRN25July",
+    databaseId: '686d24f9000e4fb75d13',
+    userCollectionId: "686d253e00196465329c",
+
 }
