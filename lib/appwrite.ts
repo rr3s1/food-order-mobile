c71981916880a4fbba17c295534495d6bd7caf2f
aessary <raressilviulazar@gmail.com>
2025-07-08 18:46:35 +0100

    Database Architecture

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -4,9 +4,14 @@ import {CreateUserParams, GetMenuParams, SignInParams} from "@/type";
 export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
-    platform: "com.aessar.FoodOrderRN25July",
-    databaseId: '686d24f9000e4fb75d13',
-    userCollectionId: "686d253e00196465329c",
+    platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!,
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!,
+    bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID!,
+    userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID!,
+    categoriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_CATEGORIES_COLLECTION_ID!,
+    menuCollectionId: process.env.EXPO_PUBLIC_APPWRITE_MENU_COLLECTION_ID!,
+    customizationsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_CUSTOMIZATIONS_COLLECTION_ID!,
+    menuCustomizationsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_MENU_CUSTOMIZATIONS_COLLECTION_ID!,
 }
 
 export const client = new Client();
