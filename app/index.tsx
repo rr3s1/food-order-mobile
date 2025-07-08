1ffa32a1eb7304045e99087d08b2828a5c1191e5
aessary <raressilviulazar@gmail.com>
2025-07-08 13:01:03 +0100

    NativeWind & Custom Fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,12 @@
+import "./globals.css"
 import { Text, View } from "react-native";
 
 export default function Index() {
     return (
-        // The View component is a fundamental container that supports layout with flexbox.
-        <View
-            style={{
-                flex: 1, // 'flex: 1' makes the View expand to fill the entire screen.
-                justifyContent: "center", // 'justifyContent' aligns children vertically in the center.
-                alignItems: "center", // 'alignItems' aligns children horizontally in the center.
-            }}
-        >
-            {/* The Text component is used to display strings of text. */}
-            <Text>Welcome to Food Ordering App!</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-5xl text-center text-primary font-quicksand-bold">
+                Welcome to Food Order App!
+            </Text>
         </View>
     );
 }
